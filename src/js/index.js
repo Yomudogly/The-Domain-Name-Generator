c93ad3e3fcf5530a2be2a0b16bc6571794f2553c
Roman Khalnepesov <khalnepesov@gmail.com>
2019-10-30 11:59:14 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,20 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let newMessage = [];
+
+  pronoun.forEach(element1 => {
+    adj.forEach(element2 => {
+      noun.forEach(element3 => {
+        newMessage.push(` ${element1}${element2}${element3}.com`);
+      });
+    });
+  });
+
+  this.console.log(newMessage);
+  document.getElementById("domains").innerHTML = newMessage;
 };
